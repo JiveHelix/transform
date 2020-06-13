023175a5b025ad128c0cd7cf655d98e640689fe5
Jive Helix <jivehelix@gmail.com>
2020-06-13 03:07:04 -0600

    Added GetInstanceVars and GetClassVars for @Transform decorated classes.

--- python/transform/transform.py
+++ python/transform/transform.py
@@ -1,16 +1,16 @@
 ##
 # @file transform.py
-# 
+#
 # @brief A class decorator that creates a new class with all of the
 # attributes of a prototype class converted to a new type.
-# 
+#
 # @author Jive Helix (jivehelix@gmail.com)
 # @date 11 Jun 2020
 # @copyright Jive Helix
 # Licensed under the MIT license. See LICENSE file.
 
 from __future__ import annotations
-from typing import TypeVar, Callable, Type, Any, Optional
+from typing import TypeVar, Callable, Type, Any, Optional, List
 import attr
 import warnings
 import types
@@ -44,7 +44,7 @@ def Transform(
         namePrefix: Optional[str] = None) -> Callable[[Type[T]], Type[T]]:
 
     hasName: bool = GetHasName(attributeMaker)
-    
+
     if hasName:
         # The attributeMaker accepts a name argument
         if namePrefix is not None:
@@ -67,6 +67,10 @@ def Transform(
 
     def TransformWrapper(class_: Type[T]) -> Type[T]:
         # Set the class vars from protoType
+        if hasattr(protoType, '__slots__'):
+            setattr(class_, '__slots__', protoType.__slots__)
+
+        classVars: List[str] = []
         for name in dir(protoType):
             if name.startswith("_"):
                 # Ignoring dunders and private names
@@ -82,9 +86,16 @@ def Transform(
             else:
                 setattr(class_, name, attributeMaker(member))
 
+            classVars.append(name)
+
+        setattr(class_, '__transform_class_vars__', classVars)
+
+        instanceVars = list(attr.fields_dict(protoType).keys())
+        setattr(class_, '__transform_vars__', instanceVars)
+
         if init:
             def __init__(self: Any, protoType: ProtoType) -> None:
-                for name in attr.fields_dict(type(protoType)):
+                for name in instanceVars:
                     setattr(self, name, Convert(name, protoType))
 
             setattr(class_, "__init__", __init__)
@@ -92,3 +103,13 @@ def Transform(
         return class_
 
     return TransformWrapper
+
+
+
+def GetInstanceVars(class_: Type[Any]) -> List[str]:
+    return class_.__transform_vars__
+
+
+def GetClassVars(class_: Type[Any]) -> List[str]:
+    return class_.__transform_class_vars__
+
